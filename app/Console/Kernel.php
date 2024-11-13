bfeeca7f185257d79812b0e3eafedfefe4c6bf28
Davit Gabashvili <datigabashvili@gmail.com>
2024-11-13 17:59:00 +0400

    check cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('app:retrieve-rates')->hourly();
+         $schedule->command('app:retrieve-rates')->everyMinute();
     }
 
     /**
