f066e2bf245611849407d05ee1ad8b6e20ebbe3d
Davit Gabashvili <datigabashvili@gmail.com>
2024-11-13 18:08:11 +0400

    every fifteen minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('app:retrieve-rates')->everyMinute();
+         $schedule->command('app:retrieve-rates')->everyFifteenMinutes();
     }
 
     /**
