f95cbbf8f8742859d94f61b64130b5a1b3790276
Davit Gabashvili <datigabashvili@gmail.com>
2025-02-25 16:50:49 +0400

    fix private number unique

--- app/Http/Requests/StoreStudentRequest.php
+++ app/Http/Requests/StoreStudentRequest.php
@@ -23,7 +23,7 @@ class StoreStudentRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:191',
-            'private_number' => 'nullable|string|max:45',
+            'private_number' => 'nullable|string|max:45|unique:students,private_number',
             'grade' => 'nullable|integer|max:12',
             'group' => 'nullable|in:ა,ბ,გ,დ,ე,ვ,ზ,თ,ი,კ,A,B,C,D,E,F,G,H,I,J,ქართული,ინგლისური',
             'sector' => 'nullable|in:ქართული,IB,ASAS,ბაღი',
--- app/Http/Requests/UpdateStudentRequest.php
+++ app/Http/Requests/UpdateStudentRequest.php
@@ -23,7 +23,7 @@ class UpdateStudentRequest extends FormRequest
     {
         return [
             'name' => 'nullable|string|max:191',
-            'private_number' => 'nullable|string|max:45',
+            'private_number' => 'nullable|string|max:45|unique:students,private_number',
             'grade' => 'nullable|integer|max:12',
             'group' => 'nullable|in:ა,ბ,გ,დ,ე,ვ,ზ,თ,ი,კ,A,B,C,D,E,F,G,H,I,J,ქართული,ინგლისური',
             'sector' => 'nullable|in:ქართული,IB,ASAS,ბაღი',
