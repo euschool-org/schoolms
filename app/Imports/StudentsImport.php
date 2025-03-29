36787c3c2c8d20444866074c263f2def84d05aef
Davit Gabashvili <datigabashvili@gmail.com>
2025-03-29 17:04:23 +0400

    cancel private number validation

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -54,7 +54,7 @@ class StudentsImport implements ToCollection, WithHeadingRow
     {
         return Validator::make($row, [
             'name' => 'required|string|max:191',
-            'private_number' => 'nullable|string|max:45|unique:students,private_number',
+            'private_number' => 'nullable|unique:students,private_number',
             'grade' => 'nullable|integer|max:12',
             'group' => 'nullable|in:ა,ბ,გ,დ,ე,ვ,ზ,თ,ი,კ,A,B,C,D,E,F,G,H,I,J,ქართული,ინგლისური',
             'sector' => 'nullable|in:ქართული,IB,ASAS,ბაღი',
